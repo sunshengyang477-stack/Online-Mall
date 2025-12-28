4a6a6177cc10b73ea3630b8c611856b571ae1499
sunshengyang477-stack <sunshengyang477@gmail.com>
2025-12-28 22:01:21 +0800

    配置依赖、初始化界面

--- onlinemall/app/build.gradle.kts
+++ onlinemall/app/build.gradle.kts
@@ -32,10 +32,19 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
+    implementation ("com.google.android.material:material:1.5.0")
+    implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation ("com.github.bumptech.glide:glide:4.13.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.13.0")
+    implementation ("androidx.appcompat:appcompat:1.4.1")
+    implementation ("com.google.code.gson:gson:2.8.8")
 }
